69d2faa5aaabff0c2747abb4e634fc984d4370f1
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-27 11:40:57 +0900

    release

--- data_platform_plant_sql_storage_location_data.sql
+++ data_platform_plant_sql_storage_location_data.sql
@@ -1,27 +1,25 @@
 CREATE TABLE `data_platform_plant_storage_location_data`
 (
-  `BusinessPartner`                 int(10) NOT NULL,
+  `BusinessPartner`                 int(12) NOT NULL,
   `Plant`                           varchar(4) NOT NULL,
   `StorageLocation`                 varchar(4) NOT NULL,
   `StorageLocationFullName`         varchar(200) DEFAULT NULL,
-  `StorageLocationName`             varchar(100) DEFAULT NULL,
-  `Language`                        varchar(2) DEFAULT NULL,
-  `CreationDate`                    varchar(80) DEFAULT NULL,
-  `CreationTime`                    varchar(80) DEFAULT NULL,
-  `LastChangeDate`                  varchar(80) DEFAULT NULL,
-  `LastChangeTime`                  varchar(80) DEFAULT NULL,
-  `SearchTerm1`                     varchar(20) DEFAULT NULL,
-  `SearchTerm2`                     varchar(20) DEFAULT NULL,
+  `StorageLocationName`             varchar(100) NOT NULL,
+  `CreationDate`                    date NOT NULL,
+  `CreationTime`                    time NOT NULL,
+  `LastChangeDate`                  date NOT NULL,
+  `LastChangeTime`                  time NOT NULL,
+  `SearchTerm1`                     varchar(40) DEFAULT NULL,
+  `SearchTerm2`                     varchar(40) DEFAULT NULL,
   `StorageLocationIsBlocked`        tinyint(1) DEFAULT NULL,
   `GroupStorageLocationName1`       varchar(40) DEFAULT NULL,
   `GroupStorageLocationName2`       varchar(40) DEFAULT NULL,
-  `IsMarkedForArchiving`            tinyint(1) DEFAULT NULL,
   `StorageLocationIDByExtSystem`    varchar(20) DEFAULT NULL,
+  `IsMarkedForDeletion`             tinyint(1) DEFAULT NULL,
   
     PRIMARY KEY (`BusinessPartner`, `Plant`, `StorageLocation`),
     
-    CONSTRAINT `DataPlatformPlantStorageLocationData_fk` FOREIGN KEY (`BusinessPartner`, `Plant`) REFERENCES `data_platform_plant_general_data` (`BusinessPartner`, `Plant`),
-    CONSTRAINT `DataPlatformPlantStorageLocationDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+    CONSTRAINT `DataPlatformPlantStorageLocationData_fk` FOREIGN KEY (`BusinessPartner`, `Plant`) REFERENCES `data_platform_plant_general_data` (`BusinessPartner`, `Plant`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
